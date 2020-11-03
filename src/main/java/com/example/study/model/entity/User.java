c8c027756db72520d189cdbef98085b1a3d616f3
DevJeong <jinung.dev@gmail.com>
2020-11-03 23:57:22 +0900

    JPA 연동 및 Entity 생성

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity // ==table
+//@Table(name = "user") // 클래스이름과 테이블 이름이 같으면 생략 가능하다.
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String account;
+
+  private String email;
+
+  private String phoneNumber;
+
+  private LocalDateTime createdAt;
+
+  private String createdBy;
+
+  private LocalDateTime updatedAt;
+
+  private String updatedBy;
+}
