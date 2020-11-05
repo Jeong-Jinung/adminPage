71dffc0e44b2de5477460655c500d11fb9c1d049
DevJeong <jinung.dev@gmail.com>
2020-11-05 23:56:21 +0900

    JPA CRUD 테스트 (read)

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @DataJpaTest  // JPA 테스트 관련 컴포넌트만 Import
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)    // 실제 db 사용
@@ -22,18 +24,23 @@ public class UserRepositoryTest {
   public void create(){
     // String sql = insert into user (%s, %d, %d) value (account, email, age);
     User user = new User();
-    user.setAccount("TestUser03");
-    user.setEmail("TestUser03@gmail.com");
+    user.setAccount("TestUser01");
+    user.setEmail("TestUser01@gmail.com");
     user.setPhoneNumber("010-3333-3333");
     user.setCreatedAt(LocalDateTime.now());
-    user.setCreatedBy("TestUser03");
+    user.setCreatedBy("TestUser01");
 
     User newUser = userRepository.save(user);
     System.out.println("newUser : " + newUser);
   }
 
+  @Test
   public void read() {
+    Optional<User> user = userRepository.findById(2L);
 
+    user.ifPresent(selectUser -> { // 있으면 출력하겠다.
+      System.out.println("user : " + selectUser);
+    });
   }
 
   public void update() {
